13e4b4cb64ca0990c1dcb1f4e42488f9bba09457
Onnion <marcio.hsdl@gmail.com>
2022-01-06 17:19:57 -0300

    refactor(issues): altera implementaçao da classe PaymentOrdersAttributes para resolver duplicidades

--- src/modules/payment-orders/classes/payment-orders-attributes.ts
+++ src/modules/payment-orders/classes/payment-orders-attributes.ts
@@ -1,19 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Transform } from 'class-transformer';
-import {
-  IsDateString,
-  IsNotEmpty,
-  IsNumber,
-  IsOptional,
-  IsString,
-  IsUUID,
-} from 'class-validator';
+import { IsEnum, IsNotEmpty, IsString, IsUUID } from 'class-validator';
+import { CreatePaymentOrderDto } from '../dto/create-payment-order.dto';
 import { TransferStatus } from '../enum/transfer-status.enum';
 import { Attributes } from '../interfaces/attributes.interface';
-import { TurnIntoCoins } from '../transforms/turn-into-coins/turn-into-coins.transform';
-import { IsValidCurrency } from '../validators/number-of-digits.validator';
 
-export abstract class PaymentOrderAttributes implements Attributes {
+export abstract class PaymentOrderAttributes
+  extends CreatePaymentOrderDto
+  implements Attributes
+{
   @ApiProperty({
     description: 'Id interno da transferência',
     example: '31654014-5b20-4301-aaa6-90401b052a6b',
@@ -24,43 +18,10 @@ export abstract class PaymentOrderAttributes implements Attributes {
   internalId: string;
 
   @ApiProperty({
-    description: 'Id externo da transferência',
-    example: 1,
+    description: 'Status da liquidação de transferência',
+    enum: TransferStatus,
   })
-  @IsNumber()
   @IsNotEmpty()
-  externalId: number;
-
+  @IsEnum(TransferStatus)
   status: TransferStatus;
-
-  @ApiProperty({
-    description: 'Quantidade em centavos da transferência (100 -> R$ 1,00)',
-    example: 100,
-  })
-  @IsNumber()
-  @IsNotEmpty()
-  @IsValidCurrency({ message: 'Número de casas decimais inválidas' })
-  @Transform(TurnIntoCoins)
-  amount: number;
-
-  @ApiProperty({
-    description: 'Data da liquidação da transferência',
-    example: '2021-12-25',
-    format: 'yyyy-MM-DD',
-  })
-  @IsDateString()
-  @IsString()
-  @IsNotEmpty()
-  expectedOn: string;
-
-  @ApiProperty({
-    description: 'Data de vencimento da transferência',
-    example: '2021-12-25',
-    format: 'yyyy-MM-DD',
-    required: false,
-  })
-  @IsDateString()
-  @IsString()
-  @IsOptional()
-  dueDate?: string;
 }
